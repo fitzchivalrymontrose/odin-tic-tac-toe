76a29f99a414a8ddf13fd4b82454913f66d3c8e9
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2022-01-15 02:09:45 -0500

    JS functions.

--- ticTacToe.js
+++ ticTacToe.js
@@ -3,19 +3,24 @@
 const gameBoard = (() => {
     const board = document.querySelector('#game-board');
     const gameSquares = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-        
 
+    function reportBoardState(){
+
+    }
+    function updateBoardState(player, position, marker){
+
+    }
 })();
 
 const displayRenderer = (() => {
 
 })();
 
-function Player(name, gamePiece){
-    const placePiece = (gamePiece) => {
-        // fill gameSquare with gamePiece (x or o)
+function Player(name, marker){
+    const placeMarker = (marker) => {
+        // fill gameSquare with marker (x or o)
     }
-    return {name, placePiece};
+    return {name, placeMarker};
 };
 
 const gameController = (() => {
