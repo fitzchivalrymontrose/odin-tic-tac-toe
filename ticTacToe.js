6084126dc95a17d159f2cd3548f841736af1eb14
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2022-01-16 18:18:22 -0500

    Added eventlisteners for placing Xs and Os.

--- ticTacToe.js
+++ ticTacToe.js
@@ -3,7 +3,15 @@
 const gameSquareValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 const gameSquares = document.querySelectorAll(".square");
+gameSquares.forEach((square) => {
+    square.addEventListener('click', placeMark);
+});
 
+function placeMark(e){
+    console.log(e.target);
+}
+
+// display gameSqareValues list values onto board squares
 gameSquareValues.forEach((square) => {
     document.querySelector(`#square-${gameSquareValues.indexOf(square)}`).textContent = square;
 });
