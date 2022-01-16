5aaf816929f469cc74e316c78a61edf66757d73b
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2022-01-16 16:43:37 -0500

    JS stuff.

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,5 +1,5 @@
 "use strict"
 
-const gameBoard = (() => {
-    const gameSquareValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-})();
+const gameSquareValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+const gameSquares = document.querySelectorAll(".square");
