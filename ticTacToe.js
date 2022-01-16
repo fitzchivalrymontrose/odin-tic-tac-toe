525d66e2d4bfb03fca48a6822a7826026437a3d0
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2022-01-16 17:55:17 -0500

    Function to populate display board.

--- ticTacToe.js
+++ ticTacToe.js
@@ -3,3 +3,8 @@
 const gameSquareValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 const gameSquares = document.querySelectorAll(".square");
+
+gameSquareValues.forEach((square) => {
+    document.querySelector(`#square-${gameSquareValues.indexOf(square)}`).textContent = square;
+});
+
