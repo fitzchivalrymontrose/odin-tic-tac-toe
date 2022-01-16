22eb118361b034089fab6fc309cb5fded5bc7ada
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2022-01-16 00:32:41 -0500

    Teardown JS structure for complete rewrite.

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,28 +1,5 @@
 "use strict"
 
 const gameBoard = (() => {
-    const board = document.querySelector('#game-board');
-    const gameSquares = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-    function reportBoardState(){
-
-    }
-    function updateBoardState(player, position, marker){
-
-    }
-})();
-
-const displayRenderer = (() => {
-
-})();
-
-function Player(name, marker){
-    const placeMarker = (marker) => {
-        // fill gameSquare with marker (x or o)
-    }
-    return {name, placeMarker};
-};
-
-const gameController = (() => {
-
+    const gameSquareValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 })();
