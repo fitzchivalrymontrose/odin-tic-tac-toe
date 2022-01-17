6c667643cf0d42f566d07184d3c669065bca8ba3
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2022-01-17 00:17:36 -0500

    JS gameBoard module.

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,18 +1,27 @@
 "use strict"
 
-const gameSquareValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// const gameSquares = document.querySelectorAll(".square");
 
-const gameSquares = document.querySelectorAll(".square");
-gameSquares.forEach((square) => {
-    square.addEventListener('click', placeMark);
-});
+// gameSquares.forEach((square) => {
+//     square.addEventListener('click', placeMark);
+// });
 
-function placeMark(e){
-    console.log(e.target);
-}
+// function placeMark(e){
+//     console.log(e.target);
+// }
 
-// display gameSqareValues list values onto board squares
-gameSquareValues.forEach((square) => {
-    document.querySelector(`#square-${gameSquareValues.indexOf(square)}`).textContent = square;
-});
+const gameBoard = (() => {
+    const gameSquareValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const gameSquares = document.querySelectorAll(".square");
 
+
+    function updateDisplay(){
+        gameSquareValues.forEach((square) => {
+            let boardSquare = document.querySelector(`#square-${gameSquareValues.indexOf(square)}`);
+            boardSquare.textContent = square;
+        });
+    }
+    return {updateDisplay}
+})();
+
+gameBoard.updateDisplay();
