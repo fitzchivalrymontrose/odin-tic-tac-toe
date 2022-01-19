aef4077009559f28c92951350e9d3f6f01131dcc
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2022-01-19 02:27:33 -0500

    Function for player to place mark on board and added favicon.

--- ticTacToe.js
+++ ticTacToe.js
@@ -22,8 +22,50 @@ test win/lose/draw
 */
 
 
-const gameSquareValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const gameSquareValues = ['', '', '', '', '', '', '', '', '', ''];
 const gameSquares = Array.from(document.querySelectorAll(".square"));
 gameSquares.forEach((square) => {
     square.textContent = gameSquareValues[square.id];
 })
+
+/*
+
+player {
+    name,
+    marker (X or O),
+
+    function placeMarker(){
+
+    }
+}
+
+*/
+let currentPlayer = 'X';
+gameSquares.forEach((square) => {
+    square.addEventListener('click', makeMark);
+});
+
+function makeMark(e){
+    if (e.target.textContent === ''){
+        e.target.textContent = currentPlayer;
+        currentPlayer = (currentPlayer) === 'X' ? 'O' : 'X';
+    }
+    checkWinLoseDraw();
+}
+
+function checkWinLoseDraw(){
+    // if win condition >>> player wins
+    // else if board full >>> draw game
+    // else continue playing
+}
+
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
